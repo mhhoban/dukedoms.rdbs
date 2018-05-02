2f83160f6ecb44f90849057af59e551f77fbe061
mhhoban <mhhoban@gmail.com>
2018-05-03 00:31:32 +0200

    added player service db to creation script

--- create_dukedoms_dbs.sh
+++ create_dukedoms_dbs.sh
@@ -8,4 +8,6 @@ echo "Creating game_service db"
 $POSTGRES <<-EOSQL
 CREATE DATABASE game_service OWNER postgres;
 CREATE DATABASE account_service OWNER postgres;
+CREATE DATABASE player_service OWNER postgres;
+
 EOSQL
